fcefa75b9c572808f338d797cb3847953f8b21e7
Thounda Craig <thoundac94@gmail.com>
2023-02-17 16:46:39 -0500

    mongoose Schema setup

--- src/models/crmModel.js
+++ src/models/crmModel.js
@@ -1,3 +1,33 @@
 /*
 This file defines the schema models for the databases
 */
+import mongoose from "mongoose";
+import mongooses from "mongoose";
+
+// Set var to accept the mongoose schema
+const Schema = mongoose.Schema;
+
+// Create mongoose schema
+export const ContactSchema = new Schema({
+  firstName: {
+    type: String,
+    required: "Enter a first name",
+  },
+  lastName: {
+    type: String,
+    required: "Enter a last name",
+  },
+  email: {
+    type: String,
+  },
+  company: {
+    type: String,
+  },
+  phone: {
+    type: Number,
+  },
+  created_date: {
+    type: Date,
+    default: Date.now,
+  },
+});
